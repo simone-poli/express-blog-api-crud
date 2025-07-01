4966e03b504e70a0000cc9ec187050f520b485eb
simone-poli <polismn97@gmail.com>
2025-07-01 14:28:27 +0200

    feat: initialize express blog API with CRUD operations for posts

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const port = 3001
+const postRouter = require("./routes/posts")
+
+
+app.use('/api/posts', postRouter)
+
+
+app.listen (port, ()=>{
+    console.log(`The server is start and listening in http://localhost:${port}`)
+})
